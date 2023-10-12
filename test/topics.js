a79f6ab1da57af942967f23d30225cbebd38379c
JamieC2002 <jamiech2002@gmail.com>
2023-10-12 15:47:16 -0400

    Added test related to question pinning. Test are ensuring correct privileges based off account

--- test/topics.js
+++ test/topics.js
@@ -33,12 +33,16 @@ describe('Topic\'s', () => {
     let topic;
     let categoryObj;
     let adminUid;
+    let instructorUid;
+    let studentUid;
     let adminJar;
     let csrf_token;
     let fooUid;
 
     before(async () => {
         adminUid = await User.create({ username: 'admin', password: '123456' });
+        instructorUid = await User.create({ username: 'instructor', password: '123456', accounttype: 'instructor' });
+        studentUid = await User.create({ username: 'student', password: '123456' });
         fooUid = await User.create({ username: 'foo' });
         await groups.join('administrators', adminUid);
         const adminLogin = await helpers.loginUser('admin', '123456');
@@ -686,6 +690,48 @@ describe('Topic\'s', () => {
 
         it('should unpin topic', async () => {
             await apiTopics.unpin({ uid: adminUid }, { tids: [newTopic.tid], cid: categoryObj.cid });
+            const pinned = await topics.getTopicField(newTopic.tid, 'pinned');
+            assert.strictEqual(pinned, 0);
+        });
+
+        it('should show isInstructor privilege for instructor is true', async () => {
+            const result = await privileges.topics.get(newTopic.tid, instructorUid);
+            assert.strictEqual(result.isInstructor, true);
+        });
+
+        it('should show isInstructor privilege for instructor is false', async () => {
+            const result = await privileges.topics.get(newTopic.tid, studentUid);
+            assert.strictEqual(result.isInstructor, false);
+        });
+
+        it('should show editable privilege for instructor is true', async () => {
+            const result = await privileges.topics.get(newTopic.tid, instructorUid);
+            assert.strictEqual(result.editable, true);
+        })
+
+        it('should show editable privilege for instructor is false', async () => {
+            const result = await privileges.topics.get(newTopic.tid, studentUid);
+            assert.strictEqual(result.isOwner, false);
+        })
+
+        it('should show isOwner privilege for owner of topic is true', async () => {
+            const result = await privileges.topics.get(newTopic.tid, adminUid);
+            assert.strictEqual(result.isOwner, true);
+        });
+
+        it('should show isOwner privilege for owner of topic is false', async () => {
+            const result = await privileges.topics.get(newTopic.tid, studentUid);
+            assert.strictEqual(result.isOwner, false);
+        })
+
+        it('should pin topic for instructor', async () => {
+            await apiTopics.pin({ uid: instructorUid }, { tids: [newTopic.tid], cid: categoryObj.cid });
+            const pinned = await topics.getTopicField(newTopic.tid, 'pinned');
+            assert.strictEqual(pinned, 1);
+        });
+
+        it('should unpin topic for instructor', async () => {
+            await apiTopics.unpin({ uid: instructorUid }, { tids: [newTopic.tid], cid: categoryObj.cid });
             const pinned = await topics.getTopicField(newTopic.tid, 'pinned');
             assert.strictEqual(pinned, 0);
         });
